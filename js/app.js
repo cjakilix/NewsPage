8f2f6f38424950c44e9c3d0b6007f39aa8631783
Cecile Jardin <cjardin@kilix.fr>
2014-11-27 18:14:58 +0100

    Slider fonction ok

--- js/app.js
+++ js/app.js
@@ -1,93 +1,103 @@
 $(document).foundation();
 
 jQuery(function($){
-    
-/*
-SLIDER
-*/
 
-var $slider = $('#slider'), // Ciblage bloc slider
-    $img = $('#slider img'), // Ciblage des balise img du slider
-    indexImg = $img.length - 1, // Définir l'index
-    i = 0, // Initilise le compteur
-    $currentImg = $img.eq(i); // Ciblage de l'image courante (index à 0)
-    console.log('img.length: ' + $img.length);
+/* SLIDER */
+    //Variable générales
+    var $slider = $('#slider'),
+    $sliderli = $('#slider li'),
+    $nbrli = $sliderli.length,
+    nbrliIndex = $nbrli -1,
+    i=0,
+    $currentItem = $('#slider li').eq(i); // le i étant réinit à 0 ci-dessus, la première image est à index 0
 
+    console.log('Nombre de li/image: ' + $nbrli);
+    console.log('Nombre de li/Index: ' + nbrliIndex);
 
-    $img.addClass("fadeout"); // Cacher les images
-    $currentImg.addClass( "fadein" ); // Afficher seulement image courante
-
-    $.each($img, function() {
+    $.each($sliderli, function() {
         $("<span id='dot" + (i+1) + "' class='dots'></span>").appendTo(".nav");
         i++;
     });
 
 
+    $sliderli.addClass('fadeout').removeClass('fadein');
+    $currentItem.addClass('fadein').removeClass('fadeout');
+    console.log('Item courrant: ' + $currentItem);
+        
 
-    $('.next').click(function(){ 
 
-        i++; // incrémente le compteur
-        $img.addClass("fadeout").removeClass("fadein"); // on cache les images
-
-        if( i > indexImg ){
-            i = 0;
-        }
-
-        $currentImg = $img.eq(i); // on définit la nouvelle image
-        $currentImg.addClass("fadein").removeClass("fadeout"); // puis on l'affiche
-
-    });
-
-    $('.prev').click(function(){
-
-        i--; // Décrémente le compteur
-        $img.addClass("fadeout").removeClass("fadein");
-
-        if( i < 0 ){
-            
-            i = indexImg;
-        }
-
-        $currentImg = $img.eq(i);
-        $currentImg.addClass("fadein").removeClass("fadeout");
-    });
-
-    $('.dots').click(function() {
+    // Navigation dots
+    $('.dots').click(function(){
 
         var numDot = $(this).attr( "id" );
-            /*console.log(numDot);*/
+        /*console.log(numDot);*/
         var elem = numDot.split('dot');
         i= elem[1];
         i= i - 1 ;
 
         /*i = 0;*/
-        $img.addClass("fadeout").removeClass("fadein");
-        $currentImg = $img.eq(i);
-        $currentImg.addClass("fadein").removeClass("fadeout");
+         $sliderli.addClass("fadeout").removeClass("fadein");
+        $currentItem = $sliderli.eq(i);
+        $currentItem.addClass("fadein").removeClass("fadeout");
 
     });
 
-    function slideImg(){
-        setTimeout(function(){
-                            
-            if(i < indexImg){ // si le compteur est inférieur au dernier index
-            i++; // on l'incrémente
-        }
-        else{ // sinon, on le remet à 0 (première image)
+
+    $('.next').click(function(){ 
+        i++; // incrémente le compteur
+        $sliderli.addClass("fadeout").removeClass("fadein"); // on cache les images
+
+        if( i > nbrliIndex ){
             i = 0;
         }
 
-        $img.addClass("fadeout").removeClass("fadein");
+        $currentItem = $sliderli.eq(i); // on définit la nouvelle image
+        $currentItem.addClass("fadein").removeClass("fadeout"); // puis on l'affiche
 
-        $currentImg = $img.eq(i);
-        $currentImg.addClass("fadein").removeClass("fadeout");
+    });   
 
 
-        slideImg();
+    $('.prev').click(function(){
 
-        }, 7000);
-    }
+        i--; // Décrémente le compteur
+        $sliderli.addClass("fadeout").removeClass("fadein");
 
+        if( i < 0 ){
+
+            i = nbrliIndex;
+        }
+
+        $currentItem = $sliderli.eq(i);
+        $currentItem.addClass("fadein").removeClass("fadeout");
+    }); 
+
+
+
+    function sliderFade(){
+        setTimeout(function(){
+
+            if(i < nbrliIndex){ // si le compteur est inférieur au dernier index
+            i++; // on l'incrémente
+            }
+            else{ // sinon, on le remet à 0 (première image)
+                i = 0;
+            }
+
+        $sliderli.addClass("fadeout").removeClass("fadein");
+
+        $currentItem = $sliderli.eq(i);
+        $currentItem.addClass("fadein").removeClass("fadeout");
+
+
+        sliderFade();
+
+        }, 3000);
+    };  // fin SLIDER
+
+sliderFade();
+
+
+/* AJAX ADD IMAGES */
 
     $(".add-image").click(function() {
 
@@ -100,53 +110,45 @@ var $slider = $('#slider'), // Ciblage bloc slider
             dataType: 'JSON',
             success : function(data, statut){ 
 
-               //Automatisation s'il n'y avait que des images
+
+                var url1 = data.image1,
+                    url2 = data.image2,
+                    url3 = data.image3;
+
+                $("<li class=\"fadeout\" ><img src='" + url1 + "' /></li>").appendTo("#slider ul");
+                $("<li class=\"fadeout\"><img src='" + url2 + "' /></li>").appendTo("#slider ul");
+                $("<li class=\"fadeout\"><img src='" + url3 + "' /></li>").appendTo("#slider ul");
+                /*console.log('Call Ajax success');*/
+
+            },// Fin success
+               
+            error : function(resultat, statut, erreur){
+                $('<span> > Une erreur s\'est produite</span>').appendTo(".add-image");
+                alert('Erreur Call Ajax');
+
+            }
+
+        }); //FIN Ajax call
+
+    });//FIN ADD IMAGES
+
+});//Fin chargement JQuery
+
+
+
+//Automatisation s'il n'y avait que des images
                /*$.each( data, function( key, val ) {
                 $("<img src='" + val + "' />" ).appendTo("#slider").addClass('fadeout'); //"<li id='" + key + "' ><img src='" + val + "' /></li>"
-                });*/
+            });*/
                 //Ajouter le nombre de dots en fontion du nombre de data
                 /*$.each( data, function( key, val ) {
                 $("<span></span>").appendTo(".nav").addClass('dots'); //"<li id='" + key + "' ><img src='" + val + "' /></li>"
-                });*/
+            });*/
 
-                //On ne récupère que les images une à une
-                var     url1 = data.image1,
-                        url2 = data.image2,
-                        url3 = data.image3;
-
-               $("<img src='" + url1 + "' />" ).appendTo("#slider").addClass('fadeout');
-               $("<img src='" + url2 + "' />" ).appendTo("#slider").addClass('fadeout');
-               $("<img src='" + url3 + "' />" ).appendTo("#slider").addClass('fadeout');
-               console.log('Call Ajax success');
 
                 // En passant par un tableau
-               
+
                /*$.each( data, function( key, val ) {
                 items.push( "<img src='" + val + "' />" ); //"<li id='" + key + "' ><img src='" + val + "' /></li>"
             });
-               $(items[1]).appendTo("#slider").addClass('fadeout');
-               $(items[2]).appendTo("#slider").addClass('fadeout');
-               slideImg();*/
-
-
-               /*$(items.join( "" )).appendTo( "#slider" );*/
-
-           },
-           // Possibilité de réjouter des cas
-           
-            error : function(resultat, statut, erreur){
-            $('<span> > Une erreur s\'est produite</span>').appendTo(".add-image");
-            alert('Erreur Call Ajax');
-
-            }/*,
-            
-        complete : function(resultat, statut){
-                
-
-            } */
-   });
-
-});
-
-
-});
+               */
